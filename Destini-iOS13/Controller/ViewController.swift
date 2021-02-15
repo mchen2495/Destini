8a297f545f5c3f56bf803744870eafff4d0435bf
michaelchen <michaelchen424@gmail.com>
2021-02-15 18:01:08 -0500

    complete Destini project

--- Destini-iOS13/Controller/ViewController.swift
+++ Destini-iOS13/Controller/ViewController.swift
@@ -14,10 +14,31 @@ class ViewController: UIViewController {
     @IBOutlet weak var choice1Button: UIButton!
     @IBOutlet weak var choice2Button: UIButton!
     
+    var storyBrain = StoryBrain()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        updateUI()
     }
+    
+    @IBAction func choiceMade(_ sender: UIButton) {
+        
+        let choice = sender.currentTitle!
+        
+        storyBrain.nextStory(userChoice: choice)
+        
+        updateUI()
+        
+    }
+    
+    
+    func updateUI(){
+        storyLabel.text = storyBrain.getStoryTitle()
+        choice1Button.setTitle(storyBrain.getChoice1(), for: .normal)
+        choice2Button.setTitle(storyBrain.getChoice2(), for: .normal)
+        
+    }
+    
 
 
 }
